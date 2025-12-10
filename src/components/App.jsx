418e0e8f479f73deaa37e463d33a5072d4acb107
Preston <prestonherr@gmail.com>
2025-12-10 14:13:23 -0600

    Added footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header/Header";
 import Home from "./Home/Home";
 import About from "./About/About";
+import Footer from "./Footer/Footer";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
